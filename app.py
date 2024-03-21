6c2eb9d7e5839dea5967ec86760dd5a1327ca426
Murali Krishna <majojukrishna@gmail.com>
2024-03-21 15:01:17 +0530

    improvements for design principles

--- app.py
+++ app.py
@@ -1,48 +1,43 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify
 from threading import Thread
 from webcrawler import WebCrawler
 from indexer import Indexer
 from ranker import Ranker
-import csv
 
 app = Flask(__name__)
 crawler = None
-indexing_complete = False  # Flag to indicate indexing status
 
 @app.route('/search', methods=['GET'])
 def search():
-    global crawler, indexing_complete
+    global crawler
     keyword = request.args.get('keyword')
     url = request.args.get('url')
-    if keyword and url:
-        if crawler is None:
-            crawler = WebCrawler()
-        crawler_thread = Thread(target=crawler.crawl, args=(url,))
-        crawler_thread.start()
-        crawler_thread.join()
-        results = index_documents(keyword)
-
-        if results:
-            return jsonify(results)
-        else:
-            return jsonify({'error': 'Result not found for the given keyword and URL'}), 404
-    else:
+    
+    if not (keyword and url):
         return jsonify({'error': 'Both keyword and URL parameters are required'}), 400
 
+    if crawler is None:
+        crawler = WebCrawler()
 
-def indexing_status():
-    global indexing_complete
-    return jsonify({'indexing_complete': indexing_complete})
+    crawler_thread = Thread(target=crawler.crawl, args=(url,))
+    crawler_thread.start()
+    crawler_thread.join()
+
+    results = index_documents(keyword)
+
+    if results:
+        return jsonify(results)
+    else:
+        return jsonify({'error': 'Result not found for the given keyword and URL'}), 404
 
 def index_documents(keyword):
-    global indexing_complete
     indexer = Indexer()
     indexer.index = crawler.index
     results = indexer.search(keyword)
+    
     if results:
         ranker = Ranker()
         ranked_results = ranker.rank_results(results, indexer.index, keyword)
-        indexing_complete = True  # Set indexing complete flag to True
         return ranked_results
     else:
         return None
